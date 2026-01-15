62aabdfbf7b263a3962d4dd11ddaa39f5e4a8c39
Jelle Simons <74780585+DevJelleS@users.noreply.github.com>
2026-01-15 10:23:07 +0100

    Add console menu and TodoService skeleton

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TodoApp.Services;
+
+var todoService = new TodoService();
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("=== TO-DO APP ===");
+    Console.WriteLine("1. Bekijk taken");
+    Console.WriteLine("2. Voeg taak toe");
+    Console.WriteLine("3. Markeer taak als voltooid");
+    Console.WriteLine("4. Verwijder taak");
+    Console.WriteLine("0. Afsluiten");
+    Console.Write("Maak een keuze: ");
+
+    var choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            todoService.ShowTodos();
+            break;
+        case "2":
+            todoService.AddTodo();
+            break;
+        case "3":
+            todoService.CompleteTodo();
+            break;
+        case "4":
+            todoService.DeleteTodo();
+            break;
+        case "0":
+            return;
+        default:
+            Console.WriteLine("Ongeldige keuze.");
+            break;
+    }
+
+    Console.WriteLine("Druk op een toets om verder te gaan...");
+    Console.ReadKey();
+}
---
+++ TodoApp/Services/TodoService.cs
@@ -0,0 +1,29 @@
+﻿using TodoApp.Models;
+
+namespace TodoApp.Services
+{
+    public class TodoService
+    {
+        private List<TodoItem> _todos = new();
+
+        public void ShowTodos()
+        {
+            Console.WriteLine("Nog geen taken.");
+        }
+
+        public void AddTodo()
+        {
+            Console.WriteLine("Toevoegen komt later.");
+        }
+
+        public void CompleteTodo()
+        {
+            Console.WriteLine("Voltooien komt later.");
+        }
+
+        public void DeleteTodo()
+        {
+            Console.WriteLine("Verwijderen komt later.");
+        }
+    }
+}
